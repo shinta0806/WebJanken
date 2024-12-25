d5ad1461e1c5f4f5c8c51d32b147b1cf6da10cd8
shinta0806 <shinta.0806@gmail.com>
2024-12-25 20:35:59 +0900

    Ver 0.15

--- webroot/public/play.vue
+++ webroot/public/play.vue
@@ -9,13 +9,13 @@
     <participantPanel v-for="participantInfo in participantInfos" :participantInfo="participantInfo"></participantPanel>
     <p>次は何を出しますか？</p>
     <p>
-        <button class="tacticsButton" @click="onTacticsGuClicked()" :disabled="isTacticsButtonDisabled">
+        <button :class="guClasses" @click="onTacticsGuClicked()" :disabled="isTacticsButtonDisabled">
             <img class="tacticsImg" src="tactics_gu.png" />
         </button>
-        <button class="tacticsButton" @click="onTacticsChokiClicked()" :disabled="isTacticsButtonDisabled">
+        <button :class="chokiClasses" @click="onTacticsChokiClicked()" :disabled="isTacticsButtonDisabled">
             <img class="tacticsImg" src="tactics_choki.png" />
         </button>
-        <button class="tacticsButton" @click="onTacticsPaClicked()" :disabled="isTacticsButtonDisabled">
+        <button :class="paClasses" @click="onTacticsPaClicked()" :disabled="isTacticsButtonDisabled">
             <img class="tacticsImg" src="tactics_pa.png" />
         </button>
     </p>
@@ -28,6 +28,10 @@
     width: 100px;
     height: 100px;
     margin-right: 20px;
+}
+
+.selectedButton {
+    background-color: #CCFFCC;
 }
 
 .tacticsImg {
@@ -62,6 +66,11 @@ export default {
             // 参加者情報群
             participantInfos: null,
 
+            // 手ボタンのスタイルクラス
+            guClasses: ["tacticsButton"],
+            chokiClasses: ["tacticsButton"],
+            paClasses: ["tacticsButton"],
+
             // 手ボタン無効化
             isTacticsButtonDisabled: false,
 
@@ -81,24 +90,25 @@ export default {
         // グーを出す
         onTacticsGuClicked() {
             // ToDo: button.@click の引数に csConstants.tactics.gu を記述すると認識されないのでワンクッション置いている
-            this.sendTactics(csConstants.tactics.gu);
+            this.sendTactics(csConstants.tactics.gu, this.guClasses);
         },
 
         // チョキを出す
         onTacticsChokiClicked() {
-            this.sendTactics(csConstants.tactics.choki);
+            this.sendTactics(csConstants.tactics.choki, this.chokiClasses);
         },
 
         // パーを出す
         onTacticsPaClicked() {
-            this.sendTactics(csConstants.tactics.pa);
+            this.sendTactics(csConstants.tactics.pa, this.paClasses);
         },
 
         // 手をサーバーに送信
-        sendTactics(tactics) {
+        sendTactics(tactics, classes) {
             // UI 処理
             this.isTacticsButtonDisabled = true;
             this.statusMessage = "他の参加者が手を出すのを待っています...";
+            classes.push("selectedButton");
 
             // 送信
             this.socket.emit(csConstants.socketEvents.selectTactics, tactics);
