b87d9fda19429f0f3abed09cafa0902addf66d67
shinta0806 <shinta.0806@gmail.com>
2025-01-01 10:54:53 +0900

    Hello, world!

---
+++ hello_world.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <p class="hello">Hello, world!</p>
+        <button @click="onButtonClicked">おまけ：{{ counter }}</button>
+    </div>
+</template>
+
+<style>
+.hello {
+    background-color: azure;
+}
+</style>
+
+<script>
+export default {
+    data() {
+        return {
+            counter: 0,
+        };
+    },
+    methods: {
+        onButtonClicked() {
+            this.counter++;
+        },
+    },
+}
+</script>
