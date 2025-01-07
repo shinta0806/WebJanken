f4d4c96ec70841be88afd39193c2c996e4fd9f0b
shinta0806 <shinta.0806@gmail.com>
2025-01-07 14:19:32 +0900

    Ver 1.08 lounge_guest コンポーネントにグループ UUID を表示するようにした。

--- webroot/public/lounge_base.vue
+++ webroot/public/lounge_base.vue
@@ -47,7 +47,6 @@ export default {
             // プレイ開始通知が来た
             this.socket.on(csConstants.socketEvents.startPlay, () => {
                 let vueApp = this.vueApp;
-                //console.log(vueApp);
                 // ToDo: socket.on イベントハンドラー解除
                 vueApp.unmount();
 
@@ -59,8 +58,6 @@ export default {
                 vueApp = Vue.createApp(Vue.defineAsyncComponent(() => loadModule("./play.vue", options)), props);
                 props["vueApp"] = vueApp;
                 vueApp.mount(document.body);
-                //console.log("ラウンジ props");
-                //console.log(props);
             });
 
             // エラー通知が来た
